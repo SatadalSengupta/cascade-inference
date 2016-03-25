38c3bf0378b974dad8f50625a095b44c2ffbcc2b
Satadal Sengupta <satadal.sengupta.nit@gmail.com>
2016-03-25 06:26:13 +0530

    Commit simulation properties

---
+++ simulation_properties.py
@@ -0,0 +1,14 @@
+import os
+
+###############################
+
+SAMPLE_SIZE = 100
+MEAN = 0.5
+SD = 0.15
+VIEW_BOOST = 0.3
+SHARE_BOOST = 0.3
+RUN_COUNT = 1
+CONTENT_COUNT = 1
+WEIGHT_THRESHOLD = 1
+
+###############################
