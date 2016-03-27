5d45393a23ab9e1c3bb9bb0f5494c795b9c290d2
Satadal Sengupta <satadal.sengupta.nit@gmail.com>
2016-03-27 08:18:42 +0530

    Add verbose display feature

---
+++ verbose_display.py
@@ -0,0 +1,21 @@
+import simulation_properties as SP
+
+##################################################
+
+def display(text):
+    if SP.VERBOSE:
+        print text
+    return
+
+##################################################
+
+def main():
+    display("Hello World")
+    return
+
+##################################################
+
+if __name__ == "__main__":
+    main()
+
+##################################################
